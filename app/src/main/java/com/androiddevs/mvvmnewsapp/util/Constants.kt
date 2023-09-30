7c3cecd2832862928c9a8bdeb3431fdc34618e90
My PC <azamatotaboev7621@gmail.com>
2023-09-30 12:42:27 +0500

    setup retrofit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "8366fc3ce12040338505bd85c80a6c2f"
+        const val BASE_URL = "https://newsapi.org/"
+    }
+}
